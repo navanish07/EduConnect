fb2ffd7132467c8746e96c84c9e218f9285d93d7
navanishan <93936040+navanish07@users.noreply.github.com>
2024-01-06 20:18:24 +0000

    added CRUD and Search features

--- EduConnect/base/views.py
+++ EduConnect/base/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.db.models import Q
 
-from .models import Room    # Import Room model from models.py
+from .models import Room, Topic    # Import Room model from models.py
 from .forms import RoomForm # Import RoomForm from forms.py
 
 # Create your views here.
@@ -15,9 +16,19 @@ from .forms import RoomForm # Import RoomForm from forms.py
 
 
 def home(request):
-    rooms = Room.objects.all()  # Get all rooms from database
+    q = request.GET.get('q') if request.GET.get('q') != None else ''    # Get query from url if it exists, else set q to empty string
+
+    rooms = Room.objects.filter(
+        Q(topic__name__icontains = q) |
+        Q(name__icontains = q) |
+        Q(description__icontains = q)
+        )  # Get all rooms from database
+    topics = Topic.objects.all() # Get all topics from database
+
     context = {
-        'rooms': rooms
+        'rooms': rooms,
+        'topics': topics,
+        'room_count': rooms.count(),
     }
     return render(request, 'base/home.html', context)
 
@@ -36,4 +47,24 @@ def createRoom(request):
             return redirect('home')
     context = {'form': form}   # Create empty context dictionary
     return render(request, 'base/room_form.html', context)
+
+
+def updateRoom(request, pk):
+    room = Room.objects.get(id=pk)  # Get room from database with id=pk (primary key)
+    form = RoomForm(instance=room)  # Create RoomForm object with room data which will be passed to template
+    if request.method == 'POST':    # If request method is POST (form submitted)
+        form = RoomForm(request.POST, instance=room)   # Create RoomForm object with POST data and room data (instance) 
+        if form.is_valid():
+            form.save()
+            return redirect('home')
+
+    context = {'form': form}
+    return render(request, 'base/room_form.html', context)
+
+def deleteRoom(request, pk):
+    room = Room.objects.get(id=pk) 
+    if request.method == 'POST':
+        room.delete()
+        return redirect('home')
+    return render(request, 'base/delete.html', {'obj': room.name})    
     
